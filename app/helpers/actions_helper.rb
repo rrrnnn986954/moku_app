c3880dd260684bd851e0163df05aceda04431cb3
rrrnnn986954 <negima6954@gmail.com>
2025-10-03 03:10:50 +0900

    修正

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,2 +1,13 @@
 module ActionsHelper
+  def japanese_category(category)
+    case category
+    when "housework" then "家事"
+    when "work"      then "仕事"
+    when "study"     then "勉強"
+    when "hobby"     then "娯楽"
+    when "rest"      then "休憩"
+    when "other"     then "その他"
+    else "未設定"
+    end
+  end
 end
