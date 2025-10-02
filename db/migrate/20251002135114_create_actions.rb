a9cfb5b81d9bb00f778127389e6dfb1ce73a5843
rrrnnn986954 <negima6954@gmail.com>
2025-10-02 22:56:16 +0900

    アクションテーブルの作成とリレーションの記述

---
+++ db/migrate/20251002135114_create_actions.rb
@@ -0,0 +1,11 @@
+class CreateActions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :actions do |t|
+      t.references :section, null: false, foreign_key: true
+      t.datetime :started_at
+      t.datetime :ended_at
+
+      t.timestamps
+    end
+  end
+end
