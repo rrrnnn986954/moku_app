169a947dc99d9640837b94a1c4bb54f610734ff5
rrrnnn986954 <negima6954@gmail.com>
2025-10-02 16:12:57 +0900

    nicknameの追加

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_02_065912) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
