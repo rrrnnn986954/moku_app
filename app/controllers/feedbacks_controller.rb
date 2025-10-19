d22c1f309b325ed03639fcf3da1ce1a708d7a05f
rrrnnn986954 <negima6954@gmail.com>
2025-10-19 17:05:14 +0900

    feedbackコントローラーの修正

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,6 +2,9 @@ class FeedbacksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @sections = current_user.sections.order(created_at: :desc)
+    # 終了しているセクションのみ取得
+    @sections = current_user.sections
+                            .where.not(ended_at: nil)     # ← 終了済みだけ
+                            .order(created_at: :desc)     # 新しい順に並べる
   end
 end
